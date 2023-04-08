cf236c2504c523562ebc9d3ad18b8890cdebf89f
K-denno <k-denno@k-denno.github.com>
2023-04-08 16:30:51 +0300

    initial commit

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -32,7 +32,7 @@ if config('MODE')=="dev":
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),,
+           'PASSWORD': config('DB_PASSWORD'),
            'HOST': 'localhost',
            'PORT': 5432,
        }
